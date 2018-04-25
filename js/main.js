b880020d0b39325f7e6faad4091afff2f0cadd5e
usuario <marta.aguilar@kairos.com>
2018-04-25 19:48:44 +0200

    js and style

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-window.addEventListener('keydown', function(e){
-  
-  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-  const key = document.querySelector(`key[data-key = "${e.keyCode}"]`);
+
+function playSound(e){
+  var audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  var key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
    
   if(!audio) return; //stop the function from running all together
   audio.currentTime = 0;
@@ -12,4 +12,13 @@ window.addEventListener('keydown', function(e){
   // setTimeout(function(){
 
   // }, 0.07);
-});
+}  
+
+function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+var keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+window.addEventListener('keydown', playSound);
